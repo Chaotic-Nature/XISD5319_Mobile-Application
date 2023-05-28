7c9602bbb05f7921f49d4ced3bdf3f9da91c140c
Cibrian <hofstacibrian@gmail.com>
2023-05-28 23:15:22 +0200

    running app pre-database

--- app/src/main/java/com/example/wilapp/AddLearnerActivity.kt
+++ app/src/main/java/com/example/wilapp/AddLearnerActivity.kt
@@ -2,16 +2,13 @@ package com.example.wilapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import com.example.wilapp.databinding.ActivityAddLearnerBinding
-import com.google.firebase.database.ktx.database
-import com.google.firebase.ktx.Firebase
 
 class AddLearnerActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityAddLearnerBinding
-    private val database = Firebase.database
-    private var wellnessMobileClinicRef = database.getReference("learners")
+    //private val database = Firebase.database
+    //private var wellnessMobileClinicRef = database.getReference("learners")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddLearnerBinding.inflate(layoutInflater)
@@ -27,22 +24,22 @@ class AddLearnerActivity : AppCompatActivity() {
             val sex = binding.sexTextField.editText?.text.toString().trim()
             val school = binding.schoolTextField.editText?.text.toString().trim()
 
-            if(!id.isNullOrBlank() && !name.isNullOrBlank() &&
-                !surname.isNullOrBlank() && !age.isNullOrBlank() && !sex.isNullOrBlank() && !school.isNullOrBlank()){
+/*  if(!id.isNullOrBlank() && !name.isNullOrBlank() &&
+      !surname.isNullOrBlank() && !age.isNullOrBlank() && !sex.isNullOrBlank() && !school.isNullOrBlank()){
 
-                wellnessMobileClinicRef.push()
-                    .setValue(LearnerModel(id, name, surname, age.toInt(), sex, school))
-                    .addOnSuccessListener{
-                        Toast.makeText(this, "Successfully added learner", Toast.LENGTH_LONG).show()
-                    }
-                    .addOnFailureListener {e ->
-                        Toast.makeText(this, "Something went wrong. $e", Toast.LENGTH_LONG).show()
-                    }
-            }
-            else{
-                Toast.makeText(this, "fields cannot be empty.",
-                    Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
+      wellnessMobileCnicRef.push()
+          .setValue(LearnerModel(id, name, surname, age.toInt(), sex, school))
+          .addOnSuccessListener{
+              Toast.makeText(this, "Successfully added learner", Toast.LENGTH_LONG).show()
+          }
+          .addOnFailureListener {e ->
+              Toast.makeText(this, "Something went wrong. $e", Toast.LENGTH_LONG).show()
+          }
+  }
+  else{
+      Toast.makeText(this, "fields cannot be empty.",
+          Toast.LENGTH_SHORT).show()
+  } */
+}
+}
 }
