21f9a05dfa647b21691a5298213ecd836f5e78fc
Cibrian <hofstacibrian@gmail.com>
2023-10-21 15:03:49 +0200

    Calculates learners age and sex based on their ID number

--- app/src/main/java/com/example/wilapp/AddLearnerActivity.kt
+++ app/src/main/java/com/example/wilapp/AddLearnerActivity.kt
@@ -7,6 +7,8 @@ import android.widget.Toast
 import com.example.wilapp.databinding.ActivityAddLearnerBinding
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import java.util.Calendar
+import java.util.Date
 
 class AddLearnerActivity : AppCompatActivity() {
 
@@ -19,22 +21,24 @@ class AddLearnerActivity : AppCompatActivity() {
         binding = ActivityAddLearnerBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.sexSpinner.adapter = populateSpinner()
+        binding.schoolsSpinner.adapter = populateSpinner(R.array.Schools)
+
         binding.saveBtn.setOnClickListener {
             binding.idTextField.error = null
             val id = binding.idTextField.editText?.text.toString().trim()
             val name = binding.nameTextField.editText?.text.toString().trim()
             val surname = binding.surnameTextField.editText?.text.toString().trim()
-            val age = binding.ageTextField.editText?.text.toString().trim()
-            val sex = binding.sexSpinner.selectedItem.toString()
-            val school = binding.schoolTextField.editText?.text.toString().trim()
+            val age = calculateAgeFromIdNumber(id)
+            val sex = determineSexFromIdNumber(id)
+            val school = binding.schoolsSpinner.selectedItem.toString()
+            val saIdRegex = """^\d{13}$""".toRegex()
 
 
             if(id.isNotBlank() && name.isNotBlank() &&
                 surname.isNotBlank() && age.isNotBlank() && sex.isNotBlank() && school.isNotBlank()
             )
             {
-                if(id.length < 13){
+                if(!id.matches(saIdRegex)){
                     binding.idTextField.error = "Must be valid ID number"
                 }
                 else{
@@ -55,13 +59,56 @@ class AddLearnerActivity : AppCompatActivity() {
             }
         }
     }
-    private fun populateSpinner(): ArrayAdapter<CharSequence> {
+    private fun populateSpinner(arrayResourceId: Int): ArrayAdapter<CharSequence> {
         val arrayAdapter = ArrayAdapter.createFromResource(
             this,
-            R.array.sexes,
-            android.R.layout.simple_spinner_item
+            arrayResourceId,
+            R.layout.spinner_list
         )
         arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         return arrayAdapter
+    }
+
+
+    /*This code determines the users sex based on their id number*/
+    private fun determineSexFromIdNumber(idNumber: String) : String{
+        val sex: String
+        val userSex = idNumber.substring(7).toInt()
+
+        sex = if(userSex in 0..4){ /*if its greater or equal to 0 and less than or equal to 4 then the user is female*/
+                    "Female"
+              }
+        else{
+            "Male"
+        }
+        return sex
+    }
+    /*This code determines the users age based on their id number*/
+    private fun calculateAgeFromIdNumber(idNumber: String): String {
+        // Extract year, month, and day from the ID number
+        val yearStr = idNumber.substring(0, 2)
+        val monthStr = idNumber.substring(2, 4)
+        val dayStr = idNumber.substring(4, 6)
+
+        // Current date
+        val currentDate = Date()
+        val calendar = Calendar.getInstance()
+        calendar.time = currentDate
+        val currentYear = calendar.get(Calendar.YEAR)
+        val currentMonth = calendar.get(Calendar.MONTH) + 1 // Month is zero-based
+        val currentDay = calendar.get(Calendar.DAY_OF_MONTH)
+
+        // Convert extracted substrings to integers
+        val birthYear = yearStr.toInt() + 2000 // Assuming 2000 as the base year
+        val birthMonth = monthStr.toInt()
+        val birthDay = dayStr.toInt()
+
+        // Calculate age
+        var age = currentYear - birthYear
+        if (currentMonth < birthMonth || (currentMonth == birthMonth && currentDay < birthDay)) {
+            age-- // Subtract 1 year if the birthday hasn't occurred yet this year
+        }
+
+        return age.toString()
     }
 }
--- app/src/main/java/com/example/wilapp/FindLearnerActivity.kt
+++ app/src/main/java/com/example/wilapp/FindLearnerActivity.kt
@@ -1,10 +1,10 @@
 package com.example.wilapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.wilapp.databinding.ActivityFindLearnerBinding
 import com.google.firebase.database.DataSnapshot
@@ -15,7 +15,7 @@ import com.google.firebase.ktx.Firebase
 
 class FindLearnerActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityFindLearnerBinding
+    private lateinit var binding: ActivityFindLearnerBinding
     private val database = Firebase.database
     private val learnerRef = database.getReference("learners")
 
@@ -23,49 +23,60 @@ class FindLearnerActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityFindLearnerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        //Hiding the results label textview.
         binding.resultsLabelTv.visibility = View.GONE
 
         binding.searchBtn.setOnClickListener {
             binding.idNumber.error = null
             val inputID = binding.idNumber.editText?.text.toString().trim()
 
-            if(inputID.isEmpty()){
+            if (inputID.isEmpty()) {
                 binding.idNumber.error = "ID number cannot be empty"
             }
-            else if(inputID.length < 13){
+            else if (inputID.length < 13) {
                 binding.idNumber.error = "Must be a valid ID number"
             }
-            else{
-                learnerRef.addValueEventListener(object: ValueEventListener {
-                    override fun onDataChange(snapshot: DataSnapshot) {
-                        val learnerList = mutableListOf<LearnerModel>()
+            else {
+                findLearner(inputID)
+            }
+        }
+    }
 
-                        for (pulledLearner in snapshot.children){
-                            val learner : LearnerModel? = pulledLearner.getValue(LearnerModel::class.java)
-                            if(learner != null && pulledLearner.key == inputID){
-                                learnerList.add(learner)
-                            }
-                        }
-                        binding.resultsLabelTv.visibility = View.VISIBLE
-                        val adapter = LearnerModelAdapter(learnerList)
-                        binding.learnerInfoRv.layoutManager = LinearLayoutManager(this@FindLearnerActivity)
-                        binding.learnerInfoRv.adapter = adapter
+    private fun findLearner(inputID: String) {
+        learnerRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val learnerList = mutableListOf<LearnerModel>()
 
-                        adapter.setOnClickListener(object : LearnerModelAdapter.OnClickListener {
-                            override fun onClick(position: Int, learner: LearnerModel) {
-                                intent = Intent(this@FindLearnerActivity,
-                                    LearnerProfileActivity::class.java)
-                                intent.putExtra("learner", learner.id)
-                                startActivity(intent)
-                            }
-                        })
+                for (pulledLearner in snapshot.children) {
+                    val learner: LearnerModel? = pulledLearner.getValue(LearnerModel::class.java)
+                    if (learner != null && pulledLearner.key == inputID) {
+                        learnerList.add(learner)
                     }
-                    override fun onCancelled(error: DatabaseError) {
-                        Toast.makeText(this@FindLearnerActivity,
-                            "Failed to read value. ${error.toException()}", Toast.LENGTH_LONG).show()
+                }
+                binding.resultsLabelTv.visibility = View.VISIBLE
+                val adapter = LearnerModelAdapter(learnerList)
+                binding.learnerInfoRv.layoutManager = LinearLayoutManager(this@FindLearnerActivity)
+                binding.learnerInfoRv.adapter = adapter
+
+                adapter.setOnClickListener(object : LearnerModelAdapter.OnClickListener {
+                    override fun onClick(position: Int, learner: LearnerModel) {
+                        intent = Intent(
+                            this@FindLearnerActivity,
+                            LearnerProfileActivity::class.java
+                        )
+                        intent.putExtra("learner", learner.id)
+                        startActivity(intent)
                     }
                 })
             }
-        }
+
+            override fun onCancelled(error: DatabaseError) {
+                Toast.makeText(
+                    this@FindLearnerActivity,
+                    "Failed to read value. ${error.toException()}", Toast.LENGTH_LONG
+                ).show()
+            }
+        })
     }
 }
