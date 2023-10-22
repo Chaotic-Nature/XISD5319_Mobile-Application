0587556a76e1efc8ff9d889299559e80936647c8
Cibrian <hofstacibrian@gmail.com>
2023-10-22 19:56:41 +0200

    Fixed the edit feature that wasnt working

--- app/src/main/java/com/example/wilapp/EditScreeningActivity.kt
+++ app/src/main/java/com/example/wilapp/EditScreeningActivity.kt
@@ -10,72 +10,76 @@ import androidx.appcompat.app.AppCompatActivity
 import com.example.wilapp.databinding.ActivityEditScreeningBinding
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 
 class EditScreeningActivity : AppCompatActivity() {
     private lateinit var binding: ActivityEditScreeningBinding
     private val database = FirebaseDatabase.getInstance()
-    private var wellnessMobileClinicRef = database.getReference("screeningQuestions")
     private lateinit var learner: String
     private val answers = mutableListOf<ScreeningQuestionsModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditScreeningBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val learnerId = intent.getStringExtra("learner")
-        val questionAnswersRef = database.reference.child("screeningQuestions").child(learnerId!!)
+        learner = intent.getStringExtra("learner") ?: ""
+
+        val questionAnswersRef = database.reference.child("screeningQuestions").child(learner)
 
         binding.saveBtn.setOnClickListener {
-            saveEditedAnswers()
+            saveEditedAnswers(questionAnswersRef)
+            intent = Intent(this, LearnerProfileActivity::class.java)
+            intent.putExtra("learner", learner)
+            startActivity(intent)
+            finish()
         }
         binding.cancelBtn.setOnClickListener {
             intent = Intent(this, LearnerProfileActivity::class.java)
-            intent.putExtra("learner", learnerId)
+            intent.putExtra("learner", learner)
             startActivity(intent)
             finish()
         }
 
-        loadAnswersFromFirebase()
+        loadAnswersFromFirebase(questionAnswersRef)
     }
 
-    private fun saveEditedAnswers() {
+    private fun saveEditedAnswers(questionAnswersRef: DatabaseReference) {
         for (i in 0 until answers.size) {
             val switch = binding.editScreeningLayout.getChildAt(i * 2 + 1) as Switch
             answers[i].answer = switch.isChecked
         }
 
         // Now, you can update the answers directly without relying on the 'id' property
-        val answersRef = wellnessMobileClinicRef.child(learner)
-        answersRef.setValue(answers)
+        questionAnswersRef.setValue(answers)
 
         // Provide feedback to the user
         Toast.makeText(this, "Answers saved successfully", Toast.LENGTH_SHORT).show()
     }
 
-    private fun loadAnswersFromFirebase() {
+    private fun loadAnswersFromFirebase(questionAnswersRef: DatabaseReference) {
         // Retrieve answers from Firebase for the specific learner
-        wellnessMobileClinicRef.child(learner)
-            .addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onDataChange(dataSnapshot: DataSnapshot) {
-                    answers.clear() // Clear existing answers
+        questionAnswersRef.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                answers.clear() // Clear existing answers
 
-                    for (snapshot in dataSnapshot.children) {
-                        val answer = snapshot.getValue(ScreeningQuestionsModel::class.java)
-                        if (answer != null) {
-                            answers.add(answer)
-                        }
+                for (snapshot in dataSnapshot.children) {
+                    val answer = snapshot.getValue(ScreeningQuestionsModel::class.java)
+                    if (answer != null) {
+                        answers.add(answer)
                     }
-
-                    // Populate the UI with answers
-                    populateUI()
                 }
 
-                override fun onCancelled(databaseError: DatabaseError) {
-                    // Handle error
-                }
-            })
+                // Populate the UI with answers
+                populateUI()
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                // Handle error
+            }
+        })
     }
 
     private fun populateUI() {
@@ -93,5 +97,4 @@ class EditScreeningActivity : AppCompatActivity() {
             editScreeningLayout.addView(switch)
         }
     }
-
 }
